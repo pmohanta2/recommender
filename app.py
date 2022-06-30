8cf9245d31aefe295916f726e4352944db5181b2
pmohanta2 <93947067+pmohanta2@users.noreply.github.com>
2022-06-30 23:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
         if image_file is not None:
             
             #img = cv2.imread(image_file)
-            img = cv2.imdecode(numpy.fromstring(image_file.read(), numpy.uint8))
+            img = cv2.imdecode(numpy.fromstring(image_file.read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
             st.image(img)
             
             if st.button("Process"):
